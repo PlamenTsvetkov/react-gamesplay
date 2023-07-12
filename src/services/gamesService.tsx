423ffb2693e1d3e93d1b81ab3c90dedc2af339f1
PlamenTsvetkov <78791397+PlamenTsvetkov@users.noreply.github.com>
2023-07-12 12:06:28 +0300

    Clean service

--- src/services/gamesService.tsx
+++ src/services/gamesService.tsx
@@ -1,5 +1,4 @@
 import {Game} from '../interfaces/game.interface';
-// const url = 'http://localhost:5000/games';
 const url = 'http://localhost:5000/games';
 
 export const getAll = () =>{
@@ -9,7 +8,6 @@ export const getAll = () =>{
 };
 
 export const getHomeGame = () =>{
-    // const currentUrl = "http://localhost:5000/games?_sort=_createdOn&_order=desc"
     const currentUrl = "http://localhost:5000/games?_sort=_createdOn&_order=desc"
     return fetch(currentUrl)
     .then(res => res.json())
